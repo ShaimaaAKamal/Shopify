dd880a404877f67b23642fdff782b4eb5736b85d
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-01 17:02:44 +0200

    finish nav item component

--- src/app/Components/Shared/nav-item/nav-item.component.ts
+++ src/app/Components/Shared/nav-item/nav-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-item',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './nav-item.component.scss'
 })
 export class NavItemComponent {
-
+@Input() isCollapsed:boolean=false;
+@Input() navItemName:string='';
+@Input() classes:string='';
 }
