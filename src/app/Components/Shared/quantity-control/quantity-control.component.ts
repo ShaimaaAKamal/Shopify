d89e5fb7e593cdbde2bf3b4f72ea9d68f1278691
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-04 07:12:23 +0200

    finsih quantity control component

---
+++ src/app/Components/Shared/quantity-control/quantity-control.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-quantity-control',
+  standalone: false,
+  templateUrl: './quantity-control.component.html',
+  styleUrl: './quantity-control.component.scss'
+})
+export class QuantityControlComponent {
+@Input() quantity:number=1;
+@Output() quantityChange = new EventEmitter<number>();
+
+updateQuantity(change: number): void {
+    const newValue = this.quantity + change;
+    if (newValue >= 1) {
+      this.quantity = newValue;
+      this.quantityChange.emit(this.quantity);
+    }
+  }
+}
