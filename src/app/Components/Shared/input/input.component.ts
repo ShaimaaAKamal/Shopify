6757985d01d4bef00f172f615afec65fdaebc6c4
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-04 07:13:56 +0200

    finish shared input component

--- src/app/Components/Shared/input/input.component.ts
+++ src/app/Components/Shared/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
-
+@Input() type:string='text';
+@Input() id:string="";
+@Input() label:string="";
+@Input() placeholder:string='';
 }
