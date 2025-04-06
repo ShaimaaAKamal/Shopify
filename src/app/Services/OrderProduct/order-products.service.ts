9af1d0e42d3ae529471b3a97855ad27760a64e69
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-07 00:59:56 +0200

    implement order products service to handle order products value

--- src/app/Services/OrderProduct/order-products.service.ts
+++ src/app/Services/OrderProduct/order-products.service.ts
@@ -5,13 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class OrderProductsService {
-  products=new BehaviorSubject<any[]>([
-  { name: 'Iphone 14 pro max', price: 200, quantity: 2 , tax:5},
-  { name: 'Samsung Galaxy S23', price: 180, quantity: 1 , tax:5},
-  { name: 'Google Pixel 7', price: 250, quantity: 3 , tax:5},
-    { name: 'Iphone 14 pro max', price: 200, quantity: 2 , tax:5},
-  { name: 'Samsung Galaxy S23', price: 180, quantity: 1 , tax:5},
-  { name: 'Google Pixel 7', price: 250, quantity: 3 , tax:5}
-]);
+  products=new BehaviorSubject<any[]>([]);
   constructor() { }
+
 }
