008cbeb5d0f1b0daae7463ee15842655d85d277e
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-07 01:03:45 +0200

    update order products component
    using order products service within it and adjust layout

--- src/app/Components/Main/home/order-products/order-products.component.ts
+++ src/app/Components/Main/home/order-products/order-products.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, ElementRef, Input, Output, ViewChild } from '@angular/core';
 import { OrderProductsService } from '../../../../Services/OrderProduct/order-products.service';
+import { OrderCalculationsService } from '../../../../Services/OrderCalculations/order-calculations.service';
 
 @Component({
   selector: 'app-order-products',
@@ -9,7 +10,8 @@ import { OrderProductsService } from '../../../../Services/OrderProduct/order-pr
 })
 export class OrderProductsComponent {
  products :any[]= [];
- constructor(private __OrderProductsService:OrderProductsService){}
+
+ constructor(private __OrderProductsService:OrderProductsService,private __OrderCalculationsService:OrderCalculationsService){}
 
  ngOnInit(): void {
  this.__OrderProductsService.products.subscribe({
@@ -17,8 +19,16 @@ export class OrderProductsComponent {
  })
 }
 
+
 updateQuantity(index:number, newQuantity:number){
       this.products[index].quantity = newQuantity;
       this.__OrderProductsService.products.next(this.products);
+}
+removeProduct(index:number){
+      this.products.splice(index, 1);
+      this.__OrderProductsService.products.next(this.products);
+}
+getTotalQuantity():number{
+  return this.__OrderCalculationsService.getTotalQuantity();
 }
 }
