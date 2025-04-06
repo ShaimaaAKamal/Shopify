700b5df1b6da2104251ddcb8fa8930f2b06ec4ca
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-07 01:00:42 +0200

    finish home component

--- src/app/Components/Main/home/home.component.ts
+++ src/app/Components/Main/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../../Services/Customer/customer.service';
+import { ProductsService } from '../../../Services/Products/products.service';
+import { OrderProductsService } from '../../../Services/OrderProduct/order-products.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+customers!:any[];
+selectedCustomer:string='';
 
+constructor(private __CustomerService:CustomerService,private __ProductsService:ProductsService,private __OrderProductsService:OrderProductsService){}
+ngOnInit(): void {
+this.__CustomerService.customers.subscribe({
+  next:customers => this.customers=customers
+})
+}
+selectedCustomerEvent(customer:string){
+  this.selectedCustomer=customer;
+}
+searchProductsByName($event:any){
+  const searchKey=$event.target.value;
+  const filteredProducts=this.__ProductsService.findProductByName(searchKey);
+}
+
+searchProductsByBarcode($event:any){
+  const searchKey=$event.target.value;
+  const filteredProducts=this.__ProductsService.findProductByBarcode(searchKey);
+  if(filteredProducts.length>0){
+    let products=this.__OrderProductsService.products.value;
+ let existingProductIndex = products.findIndex(p => p.barcode === filteredProducts[0].barcode);
+
+if (existingProductIndex > -1) {
+  products[existingProductIndex].quantity += 1;
+} else {
+  filteredProducts[0].quantity = 1;
+  products.push(filteredProducts[0]);
+}
+    this.__OrderProductsService.products.next(products);
+}}
 }
