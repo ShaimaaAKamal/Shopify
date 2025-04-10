0862e7b80acfa4368560a4077b505e7da03db3fc
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-10 15:06:55 +0200

    update select input by making it generic

--- src/app/Components/Shared/select-input/select-input.component.ts
+++ src/app/Components/Shared/select-input/select-input.component.ts
@@ -6,14 +6,33 @@ import { Component, Input, Output,EventEmitter } from '@angular/core';
   templateUrl: './select-input.component.html',
   styleUrl: './select-input.component.scss'
 })
-export class SelectInputComponent {
-@Input() label: string = '';
-@Input() id:string='';
-@Input() options:string[]=[];
-@Output() selectedOption:EventEmitter<string>=new EventEmitter<string>();
+// export class SelectInputComponent {
+// @Input() label: string = '';
+// @Input() id:string='';
+// @Input() options:string[]=[];
+// @Output() selectedOption:EventEmitter<string>=new EventEmitter<string>();
 
- onSelectionChange(event: Event): void {
+//  onSelectionChange(event: Event): void {
+//     const value = (event.target as HTMLSelectElement).value;
+//     this.selectedOption.emit(value);
+//   }
+
+export class SelectInputComponent<T extends Record<string, any>>{
+  @Input() label: string = '';
+  @Input() id: string = '';
+  @Input() options: T[] = [];
+  @Input() prop:keyof T='';
+    @Input() value:keyof T='';
+  @Output() selectedOption: EventEmitter<T> = new EventEmitter<T>();
+
+  onSelectionChange(event: Event): void {
     const value = (event.target as HTMLSelectElement).value;
-    this.selectedOption.emit(value);
+     console.log(value);
+    // Try to find the actual option object based on the selected value
+    const selected = this.options.find((opt: any) => String(opt) === value || String((opt as any)?.id) === value);
+
+    if (selected) {
+      this.selectedOption.emit(selected);
+    }
   }
 }
