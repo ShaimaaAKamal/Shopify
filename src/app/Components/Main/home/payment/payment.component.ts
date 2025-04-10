87f7e3258239c5b49f8918aeb2abfeffa30816e5
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-10 15:06:06 +0200

    update payment component

--- src/app/Components/Main/home/payment/payment.component.ts
+++ src/app/Components/Main/home/payment/payment.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { OrderCalculationsService } from '../../../../Services/OrderCalculations/order-calculations.service';
+import { OrderProductsService } from '../../../../Services/OrderProduct/order-products.service';
+import { OrdersService } from '../../../../Services/Orders/orders.service';
+import { CommonService } from '../../../../Services/Common/common.service';
 
 @Component({
   selector: 'app-home-payment',
@@ -11,6 +14,8 @@ import { OrderCalculationsService } from '../../../../Services/OrderCalculations
 export class PaymentComponent {
 
 activeInput: string = '';
+  mustBePaid:boolean=true;
+
 keypad: string[][] = [
   ['7', '8', '9'],
   ['4', '5', '6'],
@@ -29,10 +34,15 @@ private _discountValue: number = 0;
   private _Cash: number = 0;
   private _Master_Card: number = 0;
   private _Network: number = 0;
-
-  constructor(private __OrderCalculationsService:OrderCalculationsService){}
+  private code!:string;
+  constructor(private __OrderCalculationsService:OrderCalculationsService
+    ,private __OrderProductService:OrderProductsService,
+    private __OrdersService:OrdersService
+    ,private __CommonService:CommonService
+   ){}
 
   ngOnInit(): void {
+        this.code=this.generateOrderCode();
          this.paymentMethods = [
       {
         label: 'Discount',
@@ -117,5 +127,48 @@ private _discountValue: number = 0;
   }
 
   (this as any)[this.activeInput] =(newValue);
+}
+pay_Order(){
+if(this.getPaidAmount() >= this.__OrderCalculationsService.getGrossTotal())
+{  this.setoOrderDetails('paid');
+  this.mustBePaid=true;
+}
+else this.mustBePaid=false;
+}
+hold_Order(){
+    this.setoOrderDetails('hold');
+}
+cancal_Order(){
+this.EmptyOrder();
+}
+private setoOrderDetails(status:string){
+let order={
+  code:this.code,
+  customer:this.__OrderCalculationsService.invoiveCustomer,
+  salesPerson:this.__OrderCalculationsService.getSalesPerson(),
+  products:this.__OrderProductService.products.value,
+  grossTotal:this.__OrderCalculationsService.getGrossTotal(),
+  discount:this._discountValue,
+  paymentMethods:{
+     cash:this._Cash,
+      network:this._Network,
+      masterCard:this._Master_Card
+  },
+  status:status
+ }
+ if(this.__OrdersService.addNewOrder(order))
+ {     this.EmptyOrder();
+       this.code=this.generateOrderCode();
+ }
+}
+private generateOrderCode(){
+     return this.__CommonService.generate10CharCode();
+   }
+private EmptyOrder(){
+  this.__OrderProductService.products.next([]);
+  this._Cash=0;
+  this._Master_Card=0;
+  this._Network=0;
+  this._discountValue=0;
 }
 }
