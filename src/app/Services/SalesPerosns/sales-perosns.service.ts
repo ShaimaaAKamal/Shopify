a9e3187aa17083d821d6e1108b54eb548d637149
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-11 12:34:46 +0200

    Update sales-perosns.service.ts
    implement inital service function

--- src/app/Services/SalesPerosns/sales-perosns.service.ts
+++ src/app/Services/SalesPerosns/sales-perosns.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { SalesPerson } from '../../Interfaces/sales-person';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesPerosnsService {
-  salesPersons=new BehaviorSubject<any[]>([]);
-  currentSalesPerson=new BehaviorSubject<{}>({});
+  salesPersons=new BehaviorSubject<SalesPerson[]>([]);
+  currentSalesPerson=new BehaviorSubject<SalesPerson>({name:'',id:-1});
   constructor() { }
-  
-  SetCurrentSalesPerson(salesPerson:{}){
+
+  SetCurrentSalesPerson(salesPerson:SalesPerson){
     this.currentSalesPerson.next(salesPerson);
   }
 }
